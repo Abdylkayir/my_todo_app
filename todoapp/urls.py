b6d5b8d72e1eb86aeaae9aef1e4293119a9e13a3
Abdy <abdylkayir.eshmamatov@gmail.com>
2024-06-27 14:52:20 +0600

    implemented the logic of the app, added a new html template and added the sample data

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todoapp.views import home
+from todoapp.views import home, add_task
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
+    path('add/', add_task, name='add_task'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from .models import Task
 
 def home(request):
-    return render(request, 'home.html')
+    todos = Task.objects.all()
+    return render(request, 'home.html', {'todos': todos})
+
+@csrf_exempt
+def add_task(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        status = request.POST.get('status', 'new')
+        due_date = request.POST.get('due_date')
+
+        if description and status:
+            Task.objects.create(description=description, status=status, due_date=due_date)
+        return redirect('home')
+    return render(request, 'add.html')
